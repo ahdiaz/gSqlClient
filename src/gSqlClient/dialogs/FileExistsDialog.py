5a38ec294a97456553dacf6dbf8222c705d049d3
Antonio Hernández <ahdiaz@gmail.com>
2011-10-11 21:20:23 +0200

    Fixed constructors for some dialogs.

--- src/gSqlClient/dialogs/FileExistsDialog.py
+++ src/gSqlClient/dialogs/FileExistsDialog.py
@@ -28,7 +28,7 @@ gettext.textdomain('gsqlclient')
 class FileExistsDialog(Gtk.Dialog):
 
     def __init__(self, message, parent=None):
-        GObject.GObject.__init__(self, title=_("File exists"), parent=parent, flags=Gtk.DialogFlags.MODAL, buttons=None)
+        Gtk.Dialog.__init__(self, title=_("File exists"), parent=parent, flags=Gtk.DialogFlags.MODAL, buttons=None)
         self.add_button(_("Yes"), 1)
         self.add_button(_("Cancel"), 0)
         label = Gtk.Label(label=message)
--- src/gSqlClient/dialogs/ScriptErrorDialog.py
+++ src/gSqlClient/dialogs/ScriptErrorDialog.py
@@ -28,7 +28,7 @@ gettext.textdomain('gsqlclient')
 class ScriptErrorDialog(Gtk.Dialog):
 
     def __init__(self, message, parent=None):
-        GObject.GObject.__init__(self, title=_("Script error"), parent=parent, flags=Gtk.DialogFlags.MODAL, buttons=None)
+        Gtk.Dialog.__init__(self, title=_("Script error"), parent=parent, flags=Gtk.DialogFlags.MODAL, buttons=None)
         self.add_button(_("Ignore"), 2)
         self.add_button(_("Ignore all"), 1)
         self.add_button(_("Stop script"), 0)
